bdbd79db0f41e30a3236d600da7263052109ef31
ciweiandmao <lwj197791@gmail.com>
2023-11-10 21:39:58 +0800

    anything I want

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+
+public class Main extends Solution19 {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        int[][] board={{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
+        Solution19.gameOfLife(board);
+        for (int i = 0; i < board.length; i++) {
+            System.out.print("[");
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j]);
+                if (j < board[i].length - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println("]");
+        }
     }
 }
